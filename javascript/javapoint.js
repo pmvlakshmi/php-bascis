520c4b196914fc0562971aa47e2bcce66d070d59
pmvlakshmi <lakshmi.m@volkotech.in>
2023-06-24 23:04:42 +0530

    javapoint begin

---
+++ javascript/javapoint.js
@@ -0,0 +1,14 @@
+function msg(){  
+ alert("Hello Javatpoint");  
+}  
+
+
+// JavaScript Variable
+
+var a = 10;
+var b = 20;
+var c = a+b;
+
+
+var variable1 = "Hello";
+var variable2 = "World.......";
